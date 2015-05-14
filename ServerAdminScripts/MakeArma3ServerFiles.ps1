48a44444174619dee04b2b952322f0c2c677d95d
Fail Army Team <jacob.dobkins@outlook.com>
2015-05-13 23:06:39 -0700

    Added some server-admin related scripts

---
+++ ServerAdminScripts/MakeArma3ServerFiles.ps1
@@ -0,0 +1,68 @@
+﻿#Script developed by jsd1710 for Fail Army Team
+# Used to make symbolic-links to necessary directories and hard-links to files necessary for Arma 3 Server
+# operation. Modify the variables to accommodate unique setups. Run this script in the folder you wish to 
+# run the server from.
+$A3SERVERMASTER = "G:\FAILARMY\Applications\Games\ArmA3\arma3server" #Base Arma 3 Installation directory.
+$CURRENTDIR = "$(pwd)"
+
+$ARMA3FOLDERS = @("addons",
+                    "battleye",
+                    "config",
+                    "curator",
+                    "dll", 
+                    "dta",
+                    "heli",
+                    "kart",
+                    "mark",
+                    "mpmissions"
+                    )
+
+
+$ARMA3FILES = @("arma3server.exe",
+                    "ijl15.dll",
+                    "msvcr100.dll",
+                    "physx3_x86.dll",
+                    "physx3common_x86.dll", 
+                    "physx3cooking_x86.dll",
+                    "physx3gpu_x86.dll",
+                    "steam.dll",
+                    "steam_api.dll",
+                    "steam_appid.txt",
+                    "steamclient.dll",
+                    "tier0_s.dll",
+                    "vstdlib_s.dll"
+                    )
+$CUSTOMFOLDERS = @("BEC",
+                    "keys",
+                    "logs"
+                    )
+
+# Only create files and folders if they do not already exist.
+foreach ($folder in $CUSTOMFOLDERS)
+{
+    $exists = & Test-Path $CURRENTDIR\$folder
+    if (!$exists)
+    {
+        mkdir $CURRENTDIR\$folder
+    }
+    
+}
+
+foreach ($folder in $ARMA3FOLDERS)
+{
+    $exists = & Test-Path $CURRENTDIR\$folder
+    if (!$exists)
+    {
+        cmd /c mklink /j $CURRENTDIR\$folder $A3SERVERMASTER\$folder
+    }
+    
+}
+
+foreach ($file in $ARMA3FILES)
+{
+    $exists = & Test-Path $CURRENTDIR\$file
+    if (!$exists)
+    {
+        cmd /c mklink /h $CURRENTDIR\$file $A3SERVERMASTER\$file
+    }
+}
---
+++ ServerAdminScripts/StartArma3Server_#X_Epoch_EXAMPLE.ps1
@@ -0,0 +1,26 @@
+﻿$A3SERVERdir = "G:\FAILARMY\Applications\Games\ArmA3\A3_FailArmyServer01"
+$A3SERVERexe = "$A3SERVERdir\arma3server.exe"
+
+$MODSParam = "-mod=@Epoch;@EpochHive;@allinarmaterrainpack;" #TODO: Make MODSParam and MODS work as one variable.
+
+$MODS = @("@Epoch",
+            "@allinarmaterrainpack"
+            )
+$CONFIG = "-config=$A3SERVERdir\config\A3EpochServer.cfg"
+$CFG = "-cfg=$A3SERVER01dir\config\users\A3Server.cfg"
+$PROFILES = "-profiles=$A3SERVER01dir\SC"
+$PORT = "-port=2302"
+$OPTIONS = "-name=SC -maxMem=4096 -noSound -nosplash netlog=true"
+
+# Bring mods into Server directory from base Arma3 installation using sym-link.
+foreach ($mod in $MODS)
+{
+    $exists = & Test-Path $CURRENTDIR\$mod
+    if (!$exists)
+    {
+        cmd /c mklink /j $CURRENTDIR\$mod $A3SERVERMASTER\$mod
+    }
+    
+}
+
+& $A3SERVER01exe $MODSParam $CONFIG $CFG $PROFILES $PORT $OPTIONS
