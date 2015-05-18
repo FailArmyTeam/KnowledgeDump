013d03bcdb2ceeeddf19dc24c7315f6afc3bf5ba
Fail Army Team <jacob.dobkins@outlook.com>
2015-05-17 20:10:09 -0700

    Simplified MODS variable to automatically build the appropriate MODSparam variable

--- ServerAdminScripts/StartArma3Server_#X_Epoch_EXAMPLE.ps1
+++ ServerAdminScripts/StartArma3Server_#X_Epoch_EXAMPLE.ps1
@@ -1,26 +1,27 @@
 ﻿$A3SERVERdir = "G:\FAILARMY\Applications\Games\ArmA3\A3_FailArmyServer01"
 $A3SERVERexe = "$A3SERVERdir\arma3server.exe"
 
-$MODSParam = "-mod=@Epoch;@EpochHive;@allinarmaterrainpack;" #TODO: Make MODSParam and MODS work as one variable.
+$MODSParam = "-mod=" #TODO: Make MODSParam and MODS work as one variable.
 
 $MODS = @("@Epoch",
+			"@epochhive",
             "@allinarmaterrainpack"
             )
 $CONFIG = "-config=$A3SERVERdir\config\A3EpochServer.cfg"
-$CFG = "-cfg=$A3SERVER01dir\config\users\A3Server.cfg"
-$PROFILES = "-profiles=$A3SERVER01dir\SC"
+$CFG = "-cfg=$A3SERVERdir\config\users\A3Server.cfg"
+$PROFILES = "-profiles=$A3SERVERdir\SC"
 $PORT = "-port=2302"
-$OPTIONS = "-name=SC -maxMem=4096 -noSound -nosplash netlog=true"
+$OPTIONS = "-name=SC -maxMem=4096 -noSound -nosplash server=server netlog=true"
 
 # Bring mods into Server directory from base Arma3 installation using sym-link.
 foreach ($mod in $MODS)
 {
-    $exists = & Test-Path $CURRENTDIR\$mod
+    $exists = & Test-Path $A3SERVERdir\$mod
     if (!$exists)
     {
-        cmd /c mklink /j $CURRENTDIR\$mod $A3SERVERMASTER\$mod
+        cmd /c mklink /j $A3SERVERdir\$mod $A3SERVERMASTER\$mod
     }
-    
+    $MODSParam += "$mod;" 
 }
 
-& $A3SERVER01exe $MODSParam $CONFIG $CFG $PROFILES $PORT $OPTIONS
+& $A3SERVERexe $MODSParam $CONFIG $CFG $PROFILES $PORT $OPTIONS
